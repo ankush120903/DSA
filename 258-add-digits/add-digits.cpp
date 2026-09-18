1313acd9303fc2441a2026e99cfd83647dab07f3
Ankush kumar <ankush120903@gmail.com>
2026-09-18 14:27:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (7.64%) - LeetSync

---
+++ 258-add-digits/add-digits.cpp
@@ -0,0 +1,17 @@
+// class Solution {
+// public:
+//     int addDigits(int num) {
+        
+//     }
+// };
+class Solution {
+public:
+    
+    int addDigits(int num) {
+        
+        if (num == 0) {
+            return 0;
+        }
+        return (num - 1) % 9 + 1;
+    }
+};
